680b85d263455facedd16799ff2e85a285d27724
Jorge Guerra <jorgeguerra1098@gmail.com>
2019-02-12 11:13:12 -0600

    ADD
    Recursion.

---
+++ chapter-3/recursion.js
@@ -0,0 +1,10 @@
+const isEven = (n) => {
+  if ( n == 0 ) { return true }
+  else if ( n == 1 )  { return false }
+  else if ( n < 0 ) { return isEven( n * ( -1 ) ) }
+  else { return isEven( n - 2 ) }
+}
+
+console.log( isEven( 50 ) );
+console.log( isEven( 75 ) );
+console.log( isEven( -1 ) );
